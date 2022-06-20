c675002a318b87c2e8baebf3b4e2be7d9da5f86d
Viet Anh Nguyen <nvanh7@cmcglobal.vn>
2022-06-20 08:47:25 +0700

    create booking, catalog, loyalty, payment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatalogService } from './catalog/catalog.service';
+import { BookingService } from './booking/booking.service';
+import { PaymentService } from './payment/payment.service';
+import { LoyaltyService } from './loyalty/loyalty.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    CatalogService,
+    BookingService,
+    PaymentService,
+    LoyaltyService,
+  ],
 })
 export class AppModule {}
